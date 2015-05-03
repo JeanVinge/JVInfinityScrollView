c5ce1db306908c9c6fbfe815415f9ff8b4edd03a
Jean Vinge <jean_vinge@hotmail.com>
2015-05-03 14:25:20 -0300

    scrolling left and right working =)

--- JVInfinityScrollView/JVInfinityScrollView.swift
+++ JVInfinityScrollView/JVInfinityScrollView.swift
@@ -12,10 +12,12 @@ class JVInfinityScrollView: UIScrollView, UIScrollViewDelegate {
     
     var lastContentOffsetX:CGFloat
     var imageView:UIImageView?
+    var endContentOffSet:CGFloat
     
     init(frame: CGRect, photos: [UIImage?]) {
     
         lastContentOffsetX = 0
+        endContentOffSet = 0
         UIScrollView(frame: frame)
         super.init(frame: frame)
         delegate = self
@@ -23,6 +25,8 @@ class JVInfinityScrollView: UIScrollView, UIScrollViewDelegate {
         showsHorizontalScrollIndicator = false
         contentMode = UIViewContentMode.ScaleAspectFit
         addImages(photos)
+        
+        scrollRectToVisible(CGRectMake(endContentOffSet - frame.width, 0, frame.width, frame.height), animated: false)
     }
 
     required init(coder aDecoder: NSCoder) {
@@ -36,22 +40,34 @@ class JVInfinityScrollView: UIScrollView, UIScrollViewDelegate {
         for photo in photos {
             
             imageView = UIImageView(frame: CGRectMake(x, frame.origin.y, frame.size.width, frame.size.height))
+            
+            
+            if (x == 0) {
+    
+                imageView!.image = photos.last!
+                x += frame.width
+                addSubview(imageView!)
+                
+                imageView = UIImageView(frame: CGRectMake(x, frame.origin.y, frame.size.width, frame.size.height))
+                addSubview(imageView!)
+            }
+
             imageView!.image = photo
+            
             
             x += frame.width
             
             addSubview(imageView!)
         }
+
+        endContentOffSet = x
         
+        imageView = UIImageView(frame: CGRectMake(x, frame.origin.y, frame.size.width, frame.size.height))
+        imageView!.image = photos.first!
+        
+        x += frame.width
+        addSubview(imageView!)
         contentSize = CGSizeMake(x, imageView!.frame.size.height)
-    }
-    
-    func addImage() {
-        
-    }
-    
-    func scrollViewDidScroll(scrollView: UIScrollView){
-        /* Gets called when user scrolls or drags */
     }
 
     func scrollViewDidEndDecelerating(scrollView: UIScrollView){
@@ -62,20 +78,23 @@ class JVInfinityScrollView: UIScrollView, UIScrollViewDelegate {
         if (lastContentOffsetX < scrollView.contentOffset.x) {
             
             println("moving >>>>>>>>")
-        } else if (lastContentOffsetX == scrollView.contentOffset.x) {
             
-            println("im in zero")
+            if (scrollView.contentOffset.x == endContentOffSet) {
+                
+                scrollRectToVisible(CGRectMake(frame.width, 0, frame.width, frame.height), animated: false)
+            }
+            
         } else {
             
             println("moving <<<<<<<<")
+            
+            if (scrollView.contentOffset.x == 0) {
+                
+                scrollRectToVisible(CGRectMake(endContentOffSet - frame.width, 0, frame.width, frame.height), animated: false)
+            }
         }
         
         lastContentOffsetX = scrollView.contentOffset.x
 
     }
-    
-    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool){
-    }
-    
-    
 }
